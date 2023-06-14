9cdd8ce94c23ce67694802b031bd02fa30d5adb9
André Coelho <andrecoelho@hotmail.com.br>
2023-06-14 17:33:08 -0300

    Refinamento de layout

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const textArea = document.querySelector(".digiteSeuTexto");
-const mensagem = document.querySelector(".mensagem");
+const mensagem = document.querySelector("#myTextArea");
 
 function btnEncriptar() {
-  
+  mensagem.style.backgroundImage = "none";
   const textoEncriptado = encriptar(textArea.value);
   mensagem.value = textoEncriptado;
   textArea.value = "";
@@ -28,7 +28,7 @@ function encriptar(stringEncriptada) {
 }
 
 function btnDesencriptar() {
-  
+  mensagem.style.backgroundImage = "none";
   const textoEncriptado = desencriptar(textArea.value);
   mensagem.value = textoEncriptado;
   textArea.value = "";
@@ -53,13 +53,14 @@ function desencriptar(stringDesencriptada) {
 
 function btnCopiar () {
   const texto = mensagem.value;
+  mensagem.style.backgroundImage = "";
   navigator.clipboard
     .writeText(texto)
     .then(() => {
-      console.log("Texto copiado para a área de transferência: " + texto);
+      alert("Texto copiado para a área de transferência: " + texto);
     })
     .catch((error) => {
-      console.error("Erro ao copiar o texto: ", error);
+      alert("Erro ao copiar o texto: ", error);
     });
   mensagem.value = "";
 }
